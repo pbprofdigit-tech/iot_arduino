39012b5f524831e70c4a882c6521d405fc73b6a3
pbprofdigit-tech <pb.profdigit@gmail.com>
2025-12-28 20:17:31 +0100

    Arduino IOT
    Added DHT sensor support and updated JSON body structure.

--- wifi_rest_database.ino
+++ wifi_rest_database.ino
@@ -1,17 +1,19 @@
 #include <WiFiS3.h>
+#include "DHT.h"
 
 // Credenziali WiFi
 const char* ssid = "****";
 const char* pass = "****";
 
-
 char server[] = "www.miosito.it"; // Host
 int port = 80;
-
 WiFiClient client;
+#define DHTPIN 8  // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-	
+  
   pinMode(6,INPUT);
   pinMode(7,INPUT);
   pinMode(8,INPUT);
@@ -19,7 +21,6 @@ void setup() {
 
   Serial.begin(9600);
   while (!Serial);
-
 
   // Connessione WiFi
   Serial.print("Connessione a ");
@@ -40,7 +41,6 @@ void setup() {
 
 }
 
-
 void loop() {
 
   int p1,p2,p3;
@@ -48,7 +48,7 @@ void loop() {
 
   if (client.connect(server, port)) {
 
-    String message = "", s1="Spento",s2="Spento";
+    String body,s1="Spento",s2="Spento";
     p1=digitalRead(6);
     p2=digitalRead(7);
     p3=analogRead(A0);
@@ -59,8 +59,7 @@ void loop() {
     if(p2==1) { s2="Acceso"; }
 
     // Variabile che contiene i dati in json
-    const char* body = "{\"temperatura\":"+String(temp)+ ",\"potenziometro\":"+String(p3)+",\"pulsante1\":"+String(s1)+",\"pulsante2\":"+String(s2)+"}";
-
+    body = "{\"temperatura\":"+String(temp)+ ",\"potenziometro\":"+String(p3)+",\"pulsante1\":\""+s1+"\",\"pulsante2\":\""+s2+"\"}";
 
     Serial.println("Connesso al server!");
     // Invia richiesta HTTP POST
@@ -68,11 +67,10 @@ void loop() {
     client.println("Host: www.miosito.it");
     client.println("Content-Type: application/json");
     client.print("Content-Length: ");
-    client.println(strlen(body));
+    client.println(body.length());
     client.println("Connection: close");
     client.println();
     client.println(body);
-
   } else {
     Serial.println("Connessione fallita!");
   }
@@ -85,7 +83,6 @@ void loop() {
     Serial.write(c);
   }
 
-
   if (!client.connected()) {
     Serial.println("\nDisconnesso dal server.");
     client.stop();
@@ -93,7 +90,4 @@ void loop() {
   }
 
   delay(5000);
-// nessun codice
-
-
 }
