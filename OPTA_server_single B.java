d04f03235de0821b2c4f9d0a7f74a3f8d8eaa476
pbprofdigit-tech <pb.profdigit@gmail.com>
2026-04-01 20:46:54 +0200

    Finder OPTA IOT

---
+++ OPTA_server_single B.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class socket_server { 
+	public static void main(String[] a){
+		ServerStr servente = new ServerStr();
+		servente.attendi();
+		servente.comunica();
+	}
+}	
+
+public class ServerStr {
+
+		public ServerSocket server = null;
+		public Socket client=null;
+		public String stringaRicevuta=null;
+		public String stringaModificata=null;
+		public BufferedReader inDalClient;
+		public DataOutputStream outVersoClient;
+		public ServerStr () { //costruttore 
+	}
+	
+	public Socket attendi () {
+		try {
+			server =new ServerSocket(5000);
+			client=server.accept();
+			server.close();
+			inDalClient=new BufferedReader(new InputStreamReader(client.getInputStream()));
+			outVersoClient=new DataOutputStream(client.getOutputStream());
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println("Errore istanza server");
+			System.exit(1);
+		}
+		return client;
+	}
+
+	public void comunica () {
+		while (true) {
+			try {
+				stringaRicevuta = inDalClient.readLine();
+				System.out.println("Ricevuto: "+stringaRicevuta);
+			}
+			catch (Exception e) {
+				System.out.println(e.getMessage());
+				System.out.println("Errore di rete");
+				System.exit(1);
+			}
+		}
+	}
+}
